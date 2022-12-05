1e77519578f5b4c7ab5d033856642a8be7840448
sak1202 <sakshi1202patil@gmail.com>
2022-12-05 20:48:24 +0530

    max-min array

--- src/maxmin.java
+++ src/maxmin.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
 public class maxmin {
+static void swap(int a,int b, int arr[]){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+public static void main(String args[]){
+    int arr[] = new int[20];
+    System.out.println("enter the array size");
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if(n==0){
+        System.out.println("Array is empty");
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int min=arr[0];
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]<min)
+            min=arr[i];
+        else if(arr[i]>max)
+            max=arr[i];
+    }
 
+    System.out.println("Min= "+min+"\nMax= "+max);
+
+}
 }
